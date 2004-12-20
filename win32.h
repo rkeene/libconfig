22f153642db219dc2965b875cf9953778ed7b7a8
rkeene <rkeene@noemail.net>
2004-12-20 00:48:29 +0000

    Updated win32 support slightly.
    FossilOrigin-Name: c061ce704b54d63ec81a1b849dfacacb0dd71d8e

--- win32.h
+++ win32.h
@@ -14,19 +14,29 @@
 #    define HAVE_INET_ADDR 1
 #    define HAVE_SOCKET 1
 #  endif
+#  define sleep(x) Sleep((x) * 1000)
 #else
    /* MSVC++ configuration follows */
 #  undef HAVE_UNISTD_H
 #  define HAVE_STDLIB_H 1
 #  define HAVE_WINDOWS_H 1
 #  define HAVE_STDARG_H 1
+#  define HAVE_WINSVC_H 1
+#  define HAVE_WINSOCK2_H 1
+#  define HAVE_WINDOWSX_H 1
 #endif /* __MINGW32__ */
 
 #ifdef HAVE_WINDOWS_H
 #  include <windows.h>
 #endif
+#ifdef HAVE_WINDOWSX_H
+#  include <windowsx.h>
+#endif
 #ifdef HAVE_WINSOCK2_H
 #  include <winsock2.h>
+#endif
+#ifdef HAVE_WINSVC_H
+#  include <winsvc.h>
 #endif
 
 #define _USE_WIN32_ 1
